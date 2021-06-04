472f28949895a224ceb27e24686f289e499c8e25
Joel Rodriguez Rojas <jorodriguez@ihsa.mx>
2021-06-04 14:25:16 -0500

    Cambios asyn

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -4,39 +4,43 @@ const suscriptionService = require('../services/suscriptionService');
 const Joi = require('joi');
 const moment = require('moment');
 
-let RULE_EMAIL_VALIDATION = { minDomainSegments: 2,tlds: { allow: ['com', 'net'] }};
+let RULE_EMAIL_VALIDATION = { minDomainSegments: 2, tlds: { allow: ['com', 'net'] } };
 
 const schemaBody = Joi.object({
-    para : Joi.string().email(RULE_EMAIL_VALIDATION).required(),
+    para: Joi.string().email(RULE_EMAIL_VALIDATION).required(),
     asunto: Joi.string(),
     cc: Joi.string().email(RULE_EMAIL_VALIDATION).optional().allow(''),
     cco: Joi.string().email(RULE_EMAIL_VALIDATION).optional().allow(''),
-    html:Joi.string().required(),
-    api_key:Joi.string().required()
+    html: Joi.string().required(),
+    async: Joi.bool().optional(),
+    api_key: Joi.string().required()
 });
 
 const sendEmail = async (request, response) => {
-    console.log("sendEmail");
+    console.log("@sendEmail");
     const body = { email } = request.body;
 
     try {
         await schemaBody.validateAsync(body);
-      
-        const suscription = await suscriptionService.getSuscription(body.api_key);
-        
-        let returning = { process : true };
 
-        if(suscription){
-            emailService.sendEmail(body,suscription);            
-        }else{
-            returning = new ValidationException("Se requiere un api key válida.");    
-        }   
-        
+        const suscription = await suscriptionService.getSuscription(body.api_key);
+
+        let returning = { process: true };
+        if (suscription) {
+            if (body.async){
+                returning = await emailService.sendEmail(body, suscription);
+            }else{
+                emailService.sendEmailSync(body, suscription);
+            }             
+        } else {
+            returning = new ValidationException("Se requiere un api key válida.");
+        }
+
         response.status(200).json(returning);
-        
-        console.log("===|___________________|===" );
-        console.log("===| Final del proceso |===" );
-        console.log("===|̣̣̣-------------------|===" );
+
+        console.log("===|___________________|===");
+        console.log("===| Final del proceso |===");
+        console.log("===|̣̣̣-------------------|===");
     } catch (e) {
         console.log("Exception in sendEmail controller " + e);
         response.status(401).json(new ValidationException(e.details));
--- services/emailService.js
+++ services/emailService.js
@@ -1,10 +1,10 @@
 
 const nodemailer = require('nodemailer');
 const { ValidationException } = require('../exception/exeption');
-let Email = require('../models/Email');
+//let Email = require('../models/Email');
 
-const sendEmail = (correoDto, suscription) => {
-    console.log("@sendEmail" + JSON.stringify(correoDto));
+const sendEmail = async (correoDto, suscription) => {
+    console.log("@sendEmail");
     try {
 
         const { para, cc, cco, asunto, html } = correoDto;
@@ -16,8 +16,8 @@ const sendEmail = (correoDto, suscription) => {
 
         let _cc = cc ? cc : '';
         let _cco = cco ? cco : '';
-        
-        console.log("suscription " + JSON.stringify(suscription));
+
+        //let testAccount = await nodemailer.createTestAccount();     
 
         const transporter = nodemailer.createTransport(suscription);
 
@@ -29,29 +29,76 @@ const sendEmail = (correoDto, suscription) => {
             subject: asunto,
             html: html
         };
-        console.log(`Sender FROM ${suscription.fromName}`);
-        console.log("Correo para " + para);
-        console.log("Correo cc " + cc);
-        console.log("Asunto " + asunto);
+        console.log(`=>Sender FROM ${suscription.fromName}`);
+        console.log("=>Correo para " + para);
+        console.log("=>Correo cc " + cc);
+        console.log("=>Asunto " + asunto);
 
-        transporter.sendMail(mailData, function (error, info) {            
+        let sendingResponse = await transporter.sendMail(mailData);
+
+        console.log("sendingResponse " + JSON.stringify(sendingResponse));
+
+        transporter.close();
+
+        return sendingResponse;
+
+    } catch (error) {
+        guardarLog("Error", correoDto, error);
+        return new ValidationException(error);
+    }
+};
+
+const sendEmailSync = (correoDto, suscription) => {
+    console.log("@sendEmailSync");
+    try {
+
+        const { para, cc, cco, asunto, html } = correoDto;
+
+        if (para == undefined || para == '' || para == null) {
+            console.log(" NO EXISTEN CORREOS EN NINGUN CONTENEDOR (para)");
+            throw new ValidationException("el parametro para es requerido");
+        }
+
+        let _cc = cc ? cc : '';
+        let _cco = cco ? cco : '';
+
+        const transporter = nodemailer.createTransport(suscription);
+
+        const mailData = {
+            from: suscription.fromName,
+            to: para,
+            cc: _cc,
+            cco: _cco,
+            subject: asunto,
+            html: html
+        };
+        console.log(`=>Sender FROM ${suscription.fromName}`);
+        console.log("=>Correo para " + para);
+        console.log("=>Correo cc " + cc);
+        console.log("=>Asunto " + asunto);
+
+        transporter.sendMail(mailData, function (error, info) {
             if (error) {
-                guardarLog("Error ",new Email(asunto, para, cc, cco, html, suscription.id, error));                                
+                guardarLog("Error ", new Email(asunto, para, cc, cco, html, suscription.id, error));
             } else {
-                guardarLog("ENVIO - OK ",new Email(asunto, para, cc, cco, html, suscription.idr));                                                                
+                guardarLog("ENVIO - OK ", new Email(asunto, para, cc, cco, html, suscription.idr));
             }
         });
-        transporter.close();        
-    
+       
+        transporter.close();
+      
     } catch (error) {
-        
+        guardarLog("Error", correoDto, error);
+        return new ValidationException(error);
     }
 };
 
 
-const guardarLog = (action,emailObject)=>{
-    console.log(`${action} - ${JSON.stringify(emailObject)}`);        
+
+const guardarLog = (action, emailObject, error) => {
+    console.log(`${action} - ${JSON.stringify(emailObject)} ${error}`);
 };
 
 
-module.exports = { sendEmail };
+
+module.exports = { sendEmail,sendEmailSync };
